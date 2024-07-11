d3a1929d4fc09efe7359f03928e064820c061779
Reloashed <remscherr@gmail.com>
2024-07-11 11:09:47 +0200

    initial

---
+++ src/main/java/ch/axa/its/emorganisationbackend/EmOrganisationBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.axa.its.emorganisationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmOrganisationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmOrganisationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/axa/its/emorganisationbackend/EmOrganisationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.axa.its.emorganisationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmOrganisationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
