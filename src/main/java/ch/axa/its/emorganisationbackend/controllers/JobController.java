9b93961f2facf110e211811c3f6927982c2ec027
Reloashed <remscherr@gmail.com>
2024-07-11 13:18:31 +0200

    cleaned up controllers

--- src/main/java/ch/axa/its/emorganisationbackend/controllers/JobController.java
+++ src/main/java/ch/axa/its/emorganisationbackend/controllers/JobController.java
@@ -3,7 +3,6 @@ package ch.axa.its.emorganisationbackend.controllers;
 import ch.axa.its.emorganisationbackend.domain.Job;
 import ch.axa.its.emorganisationbackend.repositories.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/ch/axa/its/emorganisationbackend/controllers/PersonController.java
+++ src/main/java/ch/axa/its/emorganisationbackend/controllers/PersonController.java
@@ -1,11 +1,8 @@
 package ch.axa.its.emorganisationbackend.controllers;
 
-import ch.axa.its.emorganisationbackend.domain.Game;
 import ch.axa.its.emorganisationbackend.domain.Person;
-import ch.axa.its.emorganisationbackend.repositories.GameRepository;
 import ch.axa.its.emorganisationbackend.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
